b45db65b1e1c6b3cd477975696fcd3908875b24b
Kyle Szeto <kyl3sz3t0@gmail.com>
2017-07-26 11:56:57 -0700

    remove and insert

--- array.js
+++ array.js
@@ -80,30 +80,71 @@ class Array {
     }
 
     pop(){
+      //cant pop empty array
         if(this.length === 0) {
             throw new Error('Can not pop an empty array!');
         }
+      //get the value of the last index of the array
         const value = memory.get(this.ptr + this.length -1);
+      //subtract length by 1
         this.length --;
-
+      //return the popped element
         return value;
     }
 
     get(index){
+      //checking if index is not negative or greater than array size
         if (index < 0 || index >= this.length){
             throw new Error ('Index error');
         }
+      //returning the value in the array at a specified index
         return memory.get(this.ptr + index);
        
+    }
+    
+    insert(index, value){
+      //checking if index is not negative
+      if (index < 0 ){
+          throw new Error ('Index error');
+      }
+      //checking if length of the array >= memory size of array
+      //if it is, resize the array so you don't go out of memory
+      if(this.length >= this.sizeOfArray){
+        this._resize((this.length+1)*10);
+      }
+      //shifting all the values to the right of the index
+      memory.copy(this.ptr+index+1,this.ptr+index,this.length-index);
+      //setting the value at that index
+      memory.set(this.ptr+index,value);
+      //incrementing the length
+      this.length++;
+    }
+
+    remove(index){
+      //cant remove an element from empty array
+        if(this.length === 0) {
+            throw new Error('Can not remove from an empty array!');
+        }
+      //checking if index is not negative or greater than array size
+        if (index < 0 || index >= this.length){
+            throw new Error ('Index error');
+        }
+        //shifting all the values to the left
+        memory.copy(this.ptr+index,this.ptr+index+1,this.length-index-1);
+        //subtracting the length
+        this.length--;
     }
 }
 
 const array = new Array();
 
 array.push(5);
+array.push(10);
+array.push(11);
 
 console.log(array);
-console.log(array.get(0));
-console.log(array.pop());
-console.log(array);
+console.log(array.get(1));
+console.log(array.insert(1,4));
+console.log(array.get(1));
+
 
