bd21dfb88e967a24a64ec4a0f042f3c88118f81c
Saule BN <saulebohoney@gmail.com>
2017-07-26 11:45:50 -0400

    resize added

--- array.js
+++ array.js
@@ -1,5 +1,16 @@
 import * as memory from './memory.js';
 
 class Array {
+    constructor(){
+        this.length = 0;
+        this.ptr = memory.allocate(this.length);  
+    }
 
+    _resize(size){
+        const oldPtr = this.ptr;
+        this.ptr = memory.allocate(size);
+        memory.copy(this.ptr,oldPtr,size);
+        memory.free(oldPtr);
+    }
 }
+
