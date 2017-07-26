23fca06f53e41525b8b6376efc27ba8132137c10
Kyle Szeto <kyl3sz3t0@gmail.com>
2017-07-26 13:02:02 -0700

    finish sort

--- interviewQuestion.js
+++ interviewQuestion.js
@@ -9,4 +9,25 @@ function lessNumber(arr,number){
     return newArr;
    
 }
-console.log(lessNumber([1,2,3,4,5,6,7,8],5));
+// console.log(lessNumber([1,2,3,4,5,6,7,8],5));
+
+// function mergeArrays(arr1, arr2){
+//   let arr = [...arr1,...arr2];
+//   // let sortedArr = [];
+//   for(let i = 0; i< arr.length;i++){
+//     for(let j = i+1; j<arr.length;j++){
+//       if(arr[i]>arr[j]){
+//         const temp = arr[j];
+//         arr[j] = arr[i];
+//         arr[i] = temp;
+//       }
+//     }
+//   }
+//   return arr;
+// }
+
+function mergeArrays(arr1,arr2){
+  let arr = [...arr1,...arr2];
+  return arr.sort((a,b)=>a-b);
+}
+console.log(mergeArrays([1,3,5,7,8,9],[2,4,6,8,10]));
