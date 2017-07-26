27a8e2bcff8e985a1134f0d6f03e77f18e78da08
Kyle Szeto <kyl3sz3t0@gmail.com>
2017-07-26 14:17:38 -0700

    mergeArray done right

--- interviewQuestion.js
+++ interviewQuestion.js
@@ -1,54 +1,65 @@
-//     let newArr= [];
-//     for (let i =0; i<arr.length; i++){
-//         if(i >= number){
-//            newArr.push(i);
-//            // arr.splice(i,1);
-//         }
-//     }   
-//     return newArr;
+function lessNumber(arr,number){
+    let newArr= [];
+    for (let i =0; i<arr.length; i++){
+        if(i >= number){
+           newArr.push(i);
+           // arr.splice(i,1);
+        }
+    }   
+    return newArr;
    
-// }
+}
 // console.log(lessNumber([1,2,3,4,5,6,7,8],5));
 
-function mergeArrays(arr1, arr2){
-  let arr = [...arr1,...arr2];
-  // let sortedArr = [];
-  for(let i = 0; i< arr.length;i++){
-    for(let j = i+1; j<arr.length;j++){
-      if(arr[i]>arr[j]){
-        const temp = arr[j];
-        arr[j] = arr[i];
-        arr[i] = temp;
-      }
-    }
-  }
-  return arr;
-}
+// function mergeArrays(arr1, arr2){
+//   let arr = [...arr1,...arr2];
+//   // let sortedArr = [];
+//   for(let i = 0; i< arr.length;i++){
+//     for(let j = i+1; j<arr.length;j++){
+//       if(arr[i]>arr[j]){
+//         const temp = arr[j];
+//         arr[j] = arr[i];
+//         arr[i] = temp;
+//       }
+//     }
+//   }
+//   return arr;
+// }
+
+// function mergeArrays(arr1,arr2){
+//   let arr = [...arr1,...arr2];
+//   return arr.sort((a,b)=>a-b);
+// }
 
 function mergeArrays(arr1,arr2){
-    let arr = [...arr1,...arr2];
-    return arr.sort((a,b)=>a-b);
+  let newArr = [];
+  let d1 = 0,d2 = 0;
+  while(d1< arr1.length){
+    if(d2<arr2.length){
+      if(arr1[d1]<arr2[d2]){
+        newArr.push(arr1[d1]);
+        d1++;
+      }else if(arr1[d1]>arr2[d2]){
+        newArr.push(arr2[d2]);
+        d2++;
+      }else{
+        newArr.push(arr1[d1]);
+        newArr.push(arr2[d2]);
+        d1++;
+        d2++;
+      }
+    }else{
+      newArr.push(arr1[d1]);
+      d1++;
+    }
+  }
+  while(d2<arr2.length){
+    newArr.push(arr2[d2]);
+    d2++;
+  }
+  console.log('d1: ',d1,arr1.length);
+  console.log('d2:',d2,arr2.length);
+  return newArr;
 }
 console.log(mergeArrays([1,3,5,7,8,9],[2,4,6,8,10]));
 
-
-function removeCharacters (string,char){
-    //let remove =false;
-    let newString='';
-    for (let i = 0; i <= string.length; i++) {
-       let remove=false;
-        for (let j = 0; j< char.length; j++){
-            if (string.charAt(i) === char.charAt(j)){
-                remove =true;
-            } 
-            
-        }
-        if(remove === false){
-            //remove=false;
-            newString += string.charAt(i);
-        }
-    }
-    return newString;
-}
-console.log(removeCharacters('amojdfj','ajf'));
- 
